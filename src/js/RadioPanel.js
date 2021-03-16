2116c80cdde5ca8edcbb6080dbaec3882bfa2952
Eliot Masset <58653158+EliotMasset@users.noreply.github.com>
2021-03-16 11:09:43 +0100

    add refs

--- src/js/RadioPanel.js
+++ src/js/RadioPanel.js
@@ -4,64 +4,58 @@ class RadioPanel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.last=null;
+    this.audio = React.createRef();
+    this.playbutton = React.createRef();
+    this.pausebutton = React.createRef();
+    this.waiter = React.createRef();
   }
 
   play()
   {
-    document.getElementById("play_button").style.display="none";
-    document.getElementById("pause_button").style.display="inline-block";
-    document.getElementById("player").play();
+    this.playbutton.current.style.display="none";
+    this.pausebutton.current.style.display="inline-block";
+    this.audio.current.play();
   }
 
   pause()
   {
-    document.getElementById("play_button").style.display="inline-block";
-    document.getElementById("pause_button").style.display="none";
-    document.getElementById("player").pause();
+    this.playbutton.current.style.display="inline-block";
+    this.pausebutton.current.style.display="none";
+    this.audio.current.pause();
   }
 
   reload()
   {
         this.play();
-        document.getElementById("play_button").style.display="none";
-        document.getElementById("lds-facebook").style.display="inline-block";
-        document.getElementById("pause_button").style.display="none";
-        document.getElementById("player").load();
+        this.playbutton.current.style.display="none";
+        this.waiter.current.style.display="inline-block";
+        this.pausebutton.current.style.display="none";
+        this.audio.current.load();
   }
 
   render() {
+      console.log(this.audio.current);
     if(this.props.radio==undefined)
     {
-        this.last=null;
-        if(document.getElementById("player")!=undefined)
-          document.getElementById("player").pause();
         return (
           <div id="RadioPanel">
-            <audio preload="auto" controls id="player"></audio>
+            <audio ref={this.audio} preload="auto" id="player"></audio>
           </div>
         );
     }
-    if(this.last!=this.props.radio.name && this.last!=null)
-    {
-        this.pause();
-        document.getElementById("play_button").style.display="none";
-        document.getElementById("lds-facebook").style.display="inline-block";
-    }
-    document.getElementById("player").addEventListener("canplaythrough", event => {
+    this.audio.current.addEventListener("canplaythrough", event => {
         /* the audio is now playable; play it if permissions allow */
-        document.getElementById("lds-facebook").style.display="none";
+        this.waiter.current.style.display="none";
         this.play();
       });
-    this.last=this.props.radio.name;
     return (
       <div id="RadioPanel">
-            <audio preload="auto" controls id="player" src={this.props.radio.url}></audio>
+            <audio ref={this.audio} preload="auto" id="player" src={this.props.radio.url}></audio>
             <img src={this.props.img} id={this.props.radio.name}></img>
             <h4>{this.props.radio.name}</h4>
-            <div onClick={() => this.play()} id="play_button"><i className="fas fa-play-circle"></i></div>
-            <div onClick={() => this.pause()} id="pause_button"><i className="fas fa-pause-circle"></i></div>
-            <div id="lds-facebook"><div></div><div></div><div></div></div>
+            <div ref={this.playbutton} onClick={() => this.play()} id="play_button"><i className="fas fa-play-circle"></i></div>
+            <div ref={this.pausebutton} onClick={() => this.pause()} id="pause_button"><i className="fas fa-pause-circle"></i></div>
+            <div ref={this.waiter} id="lds-facebook"><div></div><div></div><div></div></div>
             <div onClick={() => this.reload()} id="load_button"><i className="fas fa-redo"></i></div>
             {this.props.radio.tags.map((item) => <div key={item} className="box-tag">{item}</div>)}
       </div>
